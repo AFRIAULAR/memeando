3b122b1c54dac1f9cd4383fa4b01ba6530dd9fc0
AFRIAULAR <aularafrica@gmail.com>
2023-07-15 15:44:06 -0300

    modifying download function

--- script.js
+++ script.js
@@ -48,8 +48,8 @@ textColorSelect.addEventListener('change', () => {
 downloadButton.addEventListener('click', () => {
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
-    canvas.width = memeImage.width;
-    canvas.height = memeImage.height;
+    canvas.width = 500;
+    canvas.height = 500;
 
     context.drawImage(memeImage, 0, 0);
 
